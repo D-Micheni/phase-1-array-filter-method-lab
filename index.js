f3e874e9a6dbde64069040daebf8c95c08aa0d73
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-08-21 09:49:30 +0300

    commit tree bdabd8fa326391afe0fc96fc002c595567836a72

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching( driver, passednamed){
+    return (driver.filter(driver=> driver.toLowerCase() === passednamed.toLowerCase()))
+}
+function fuzzyMatch(driver, passedNamed) {
+    return (driver.filter(driver=>driver.toLowerCase().indexOf(passedNamed.toLowerCase())===0))
+
+}function matchName(driver ,passedName) {
+    return (driver.filter(driver=>driver.name===passedName))
+} 
